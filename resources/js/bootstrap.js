87a8d71bce94fc852f464aff3c52895b862122f3
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-11-19 11:29:59 +0530

    changed url

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,5 +39,8 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssPort: 6001,
+    enabledTransports: ["ws", "wss"],
+    encrypted: true,
     disableStats: true
 });
