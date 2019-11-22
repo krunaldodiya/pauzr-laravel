241de792dae14dd108b37a101bc5c60250178069
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-11-22 21:13:24 +0530

    added subscription

--- app/GraphQL/Subscriptions/TestSubscription.php
+++ app/GraphQL/Subscriptions/TestSubscription.php
@@ -2,22 +2,22 @@
 
 namespace App\GraphQL\Subscriptions;
 
-use Illuminate\Http\Request;
 use GraphQL\Type\Definition\ResolveInfo;
-use Nuwave\Lighthouse\Subscriptions\Subscriber;
+use Illuminate\Http\Request;
 use Nuwave\Lighthouse\Schema\Types\GraphQLSubscription;
+use Nuwave\Lighthouse\Subscriptions\Subscriber;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class TestSubscription extends GraphQLSubscription
 {
-    public function authorize(Subscriber $subscriber, Request $request): bool
+    public function authorize(Subscriber $subscriber, Request $request)
     {
         return true;
     }
 
-    public function filter(Subscriber $subscriber, $root): bool
+    public function filter(Subscriber $subscriber, $root)
     {
-        return false;
+        return true;
     }
 
     public function resolve($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
