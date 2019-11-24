e33c2ca0bb34a624121b6d5d5c3285b4ee6bf2bd
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-11-24 09:57:23 +0530

    added laravel websocket

--- app/GraphQL/Queries/HelloThere.php
+++ app/GraphQL/Queries/HelloThere.php
@@ -3,15 +3,12 @@
 namespace App\GraphQL\Queries;
 
 use GraphQL\Type\Definition\ResolveInfo;
-use Nuwave\Lighthouse\Execution\Utils\Subscription;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class HelloThere
 {
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        Subscription::broadcast('helloThere', 'hello people');
-
-        return 'hello people';
+        return $args['text'];
     }
 }
