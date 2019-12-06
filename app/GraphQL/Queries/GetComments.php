e6b05f2cea40653f0659387b7cafe0df0b8895c1
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-12-06 09:31:33 +0530

    added post comments query

---
+++ app/GraphQL/Queries/GetComments.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Comment;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class GetComments
+{
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        return Comment::with('replies')->where('id', $args['post_id']);
+    }
+}
