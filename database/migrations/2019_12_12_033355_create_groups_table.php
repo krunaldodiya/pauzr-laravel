6f547c7c8b9d0f352d68fe102e8606acca5b1343
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-12-17 09:55:00 +0530

    added group type

--- database/migrations/2019_12_12_033355_create_groups_table.php
+++ database/migrations/2019_12_12_033355_create_groups_table.php
@@ -20,12 +20,14 @@ class CreateGroupsTable extends Migration
             $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->string('name');
-            $table->text('description')->nullable();
             $table->string('photo')->nullable();
+            $table->text('description')->nullable();
             $table->boolean('status')->default(true);
 
             $table->boolean('anyone_can_post')->default(false);
             $table->boolean('anyone_can_join')->default(false);
+
+            $table->enum('type', ['Fun', 'Focus'])->default('Focus');
 
             $table->timestamps();
         });
