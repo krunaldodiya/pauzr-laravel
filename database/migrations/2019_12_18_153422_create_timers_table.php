d7f1caa0d2248de4952538b181654bc11d742d50
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-12-19 08:29:36 +0530

    added points and timers migration

---
+++ database/migrations/2019_12_18_153422_create_timers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->uuid('country_id');
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+
+            $table->integer('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timers');
+    }
+}
