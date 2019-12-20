b7a6e0b7382dc18878a41d1c5c3adc7726a8e866
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-12-20 10:07:30 +0530

    fixed typo

--- database/migrations/2019_12_18_153436_create_earnings_table.php
+++ database/migrations/2019_12_18_153436_create_earnings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePointsTable extends Migration
+class CreateEarningsTable extends Migration
 {
     /**
      * Run the migrations.
