a3bd8d67a75e4f7a2fdbe21bc6a9c22593178b3c
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-02-10 11:51:12 +0530

    added quote nova

--- app/GraphQL/Mutations/Login.php
+++ app/GraphQL/Mutations/Login.php
@@ -32,7 +32,8 @@ class Login
                 return $this->userRepository->createToken($user, $token);
             }
 
-            throw new InvalidCredentials("Login Failed", "Invalid Credentials");
+            return response(['validation' => ['username' => ['Invalid Credentials']]], 402);
+            // throw new InvalidCredentials("Login Failed", "Invalid Credentials");
         } catch (\Throwable $th) {
             throw new InvalidCredentials("Login Failed", "Invalid Credentials");
         }
